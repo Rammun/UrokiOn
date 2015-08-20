a8b090481f2ae6b0f6f01ff08ea92dab862f649a
eRKa <rlikh75@mail.ru>
2015-08-21 02:01:07 +0400

    Реализовал постраничный вывод новостей через ajax

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PagedList;
+using Domain.Entities;
+using System.Net;
 
 namespace Web.Controllers
 {
@@ -15,8 +17,15 @@ namespace Web.Controllers
         public ActionResult Index(int? page)
         {
             var group = db.Groups.FirstOrDefault(x => x.Name == "Админы");
-            var news = group == null? null: group.GroupNewses.Where(x => x.Status).ToList().ToPagedList(page?? 1, 3);
-            return View(news);
+            //var news = group == null? null: group.GroupNewses.Where(x => x.Status).ToList().ToPagedList(page?? 1, 3);
+            //return View(news);
+
+            //var listPaged = GetPagedNames(page); // GetPagedNames is found in BaseController
+            //if (listPaged == null)
+            //    return HttpNotFound();
+            ViewBag.News = group == null ? null : group.GroupNewses.Where(x => x.Status).ToList().ToPagedList(page ?? 1, 3);
+
+            return Request.IsAjaxRequest() ? (ActionResult)PartialView("_GroupNewsPartial") : View();
         }
 
         public ActionResult About()
@@ -31,6 +40,16 @@ namespace Web.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult GroupNewsAjax(List<GroupNewse> groupNews)
+        {
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+
+            return PartialView("_GroupNewsPartial", groupNews);
         }
     }
 }
